6bfd4bf834ce9ca6d5f99e17894e204b9ef4a535
Eric Hellmuth <hellmuth07@gmail.com>
2020-03-10 17:39:07 -0400

    Assignment 17 completed

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -1,19 +1,37 @@
 {
+  //Play/Pause Song
   $('button#play-pause').on('click', function() {
     player.playPause();
     $(this).attr('playState', player.playState);
   });
 
+//Next Song
   $('button#next').on('click', function() {
-if (player.playState !== 'playing') { return; }
+    if (player.playState !== 'playing') { return; }
 
-const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
-const nextSongIndex = currentSongIndex + 1;
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+    const nextSongIndex = currentSongIndex + 1;
 
-if (nextSongIndex >= album.songs.length) {
-  return;
-}
-const nextSong = album.songs[nextSongIndex];
-player.playPause(nextSong);
+    if (nextSongIndex >= album.songs.length) {
+      return;
+    }
+    const nextSong = album.songs[nextSongIndex];
+    player.playPause(nextSong);
   });
+
+//Previous Song
+  $('button#previous').on('click', function() {
+    if (player.playState !== 'playing') { return; }
+
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+    const previousSongIndex = currentSongIndex - 1;
+
+    if (previousSongIndex === -1) {
+      return;
+    }
+    const previousSong = album.songs[previousSongIndex];
+    player.playPause(previousSong);
+  })
+
+
 }
